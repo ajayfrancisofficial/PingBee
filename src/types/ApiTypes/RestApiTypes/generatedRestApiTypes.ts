4744ee59470ce92f947fb45876b8a2d71c0e843c
Ajay Francis <ajayfrancisofficial@gmail.com>
2026-05-03 00:10:40 +0530

    feat: add is_deleted_for_everyone field to Message type and include debug logging in ChatScreen and messageController

--- src/types/ApiTypes/RestApiTypes/generatedRestApiTypes.ts
+++ src/types/ApiTypes/RestApiTypes/generatedRestApiTypes.ts
@@ -337,6 +337,8 @@ export interface components {
       created_at: string;
       /** Is Read */
       is_read: boolean;
+      /** Is Deleted For Everyone */
+      is_deleted_for_everyone: boolean;
     };
     /** MessageList */
     MessageList: {
