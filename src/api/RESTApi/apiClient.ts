fea675781f269f82859cf8b1a760ee790938dd87
Ajay Francis <ajayfrancisofficial@gmail.com>
2026-06-21 21:41:56 +0530

    feat: update allowed endpoints

--- src/api/RESTApi/apiClient.ts
+++ src/api/RESTApi/apiClient.ts
@@ -31,6 +31,7 @@ export const OFFLINE_SUPPORTED_ENDPOINTS = [
   ENDPOINTS.CHATS.MESSAGES, // '/messages'
   ENDPOINTS.CHATS.USER_DETAILS, // '/chat-users-details'
   ENDPOINTS.NOTIFICATIONS.FCM_TOKEN, // '/fcm-token'
+  ENDPOINTS.PINGY.DETAILS, // '/pingy-details'
 ];
 
 // Response interceptor: auto-refresh token if 401 triggers & global error handling
